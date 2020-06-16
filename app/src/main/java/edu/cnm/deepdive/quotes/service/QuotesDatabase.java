6bac9676282a428f263844cd6a0d78cd2dcbc4e9
Nick Bennett <nick@nickbenn.com>
2020-06-16 17:05:14 -0600

    Simple pre-load.

--- app/src/main/java/edu/cnm/deepdive/quotes/service/QuotesDatabase.java
+++ app/src/main/java/edu/cnm/deepdive/quotes/service/QuotesDatabase.java
@@ -1,13 +1,16 @@
 package edu.cnm.deepdive.quotes.service;
 
 import android.app.Application;
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 import edu.cnm.deepdive.quotes.model.dao.QuoteDao;
 import edu.cnm.deepdive.quotes.model.dao.SourceDao;
 import edu.cnm.deepdive.quotes.model.entity.Quote;
 import edu.cnm.deepdive.quotes.model.entity.Source;
+import io.reactivex.schedulers.Schedulers;
 
 @Database(
     entities = {Source.class, Quote.class},
@@ -36,7 +39,29 @@ public abstract class QuotesDatabase extends RoomDatabase {
 
     private static final QuotesDatabase INSTANCE =
         Room.databaseBuilder(context, QuotesDatabase.class, DB_NAME)
-            // Set other options for builder to use when creating QuotesDatabase instance.
+            .addCallback(new Callback() {
+              @Override
+              public void onCreate(@NonNull SupportSQLiteDatabase db) {
+                super.onCreate(db);
+                QuotesDatabase database = QuotesDatabase.getInstance();
+                SourceDao sourceDao = database.getSourceDao();
+                Source source = new Source();
+                source.setName("Albert Einstein");
+                sourceDao.insert(source)
+                    .subscribeOn(Schedulers.io())
+                    .map((sourceId) -> {
+                      Quote quote = new Quote();
+                      quote.setSourceId(sourceId);
+                      quote.setText("I would teach peace rather than war. I would inculcate love rather than hate.");
+                      QuoteDao quoteDao = database.getQuoteDao();
+                      quoteDao.insert(quote)
+                          .subscribeOn(Schedulers.io())
+                          .subscribe();
+                      return sourceId;
+                    })
+                    .subscribe();
+              }
+            })
             .build();
 
   }
