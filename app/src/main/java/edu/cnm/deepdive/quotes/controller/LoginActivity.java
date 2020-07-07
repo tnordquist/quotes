f9a64db3fb4a04e58e6bac17e79fc2084661ed36
Nick Bennett <nick@nickbenn.com>
2020-07-07 14:50:10 -0600

    Integrated Google Sign In.

--- app/src/main/java/edu/cnm/deepdive/quotes/controller/LoginActivity.java
+++ app/src/main/java/edu/cnm/deepdive/quotes/controller/LoginActivity.java
@@ -1,14 +1,48 @@
 package edu.cnm.deepdive.quotes.controller;
 
+import android.content.Intent;
+import android.widget.Toast;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import edu.cnm.deepdive.quotes.R;
+import edu.cnm.deepdive.quotes.service.GoogleSignInService;
 
 public class LoginActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+  private static final int LOGIN_REQUEST_CODE = 1000;
+
+  private GoogleSignInService service;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    service = GoogleSignInService.getInstance();
+    service.refresh()
+        .addOnSuccessListener((account) -> switchToMain())
+        .addOnFailureListener((throwable) -> {
+          setContentView(R.layout.activity_login);
+          findViewById(R.id.sign_in).setOnClickListener((v) ->
+              service.startSignIn(this, LOGIN_REQUEST_CODE));
+        });
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    if (requestCode == LOGIN_REQUEST_CODE) {
+      service.completeSignIn(data)
+          .addOnSuccessListener((account) -> switchToMain())
+          .addOnFailureListener((throwable) ->
+              Toast.makeText(this, R.string.login_failure, Toast.LENGTH_LONG).show());
+    } else {
+      super.onActivityResult(requestCode, resultCode, data);
     }
+  }
+
+  private void switchToMain() {
+    Intent intent = new Intent(this, MainActivity.class);
+    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+    startActivity(intent);
+  }
+
 }
--- app/src/main/java/edu/cnm/deepdive/quotes/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/quotes/controller/MainActivity.java
@@ -1,8 +1,13 @@
 package edu.cnm.deepdive.quotes.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModelProvider;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -10,14 +15,43 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import edu.cnm.deepdive.quotes.R;
+import edu.cnm.deepdive.quotes.service.GoogleSignInService;
 import edu.cnm.deepdive.quotes.viewmodel.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
+
+  private GoogleSignInService signInService;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    setupNavigation();
+    setupObservers();
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    super.onCreateOptionsMenu(menu);
+    getMenuInflater().inflate(R.menu.options, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    boolean handled = true;
+    //noinspection SwitchStatementWithTooFewBranches
+    switch (item.getItemId()) {
+      case R.id.sign_out:
+        signInService.signOut().addOnCompleteListener((ignored) -> switchToLogin());
+        break;
+      default:
+        handled = super.onOptionsItemSelected(item);
+    }
+    return handled;
+  }
+
+  private void setupNavigation() {
     BottomNavigationView navView = findViewById(R.id.nav_view);
     // Passing each menu ID as a set of Ids because each
     // menu should be considered as top level destinations.
@@ -27,13 +61,23 @@ public class MainActivity extends AppCompatActivity {
     NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
     NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
     NavigationUI.setupWithNavController(navView, navController);
+  }
+
+  private void setupObservers() {
     MainViewModel viewModel = new ViewModelProvider(this).get(MainViewModel.class);
     getLifecycle().addObserver(viewModel);
     viewModel.getThrowable().observe(this, (throwable) -> {
       if (throwable != null) {
         Toast.makeText(this, throwable.getMessage(), Toast.LENGTH_LONG).show();
       }
-    }); 
+    });
+    signInService = GoogleSignInService.getInstance();
+  }
+
+  private void switchToLogin() {
+    Intent intent = new Intent(this, LoginActivity.class);
+    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+    startActivity(intent);
   }
 
 }
