0c4dfe83f146c92622ec795711635aaa92de0de2
Nick Bennett <nick@nickbenn.com>
2020-07-20 11:43:38 -0600

    Updated DDL raw and Markdown.

--- docs/sql/ddl.sql
+++ docs/sql/ddl.sql
@@ -9,6 +9,7 @@ CREATE UNIQUE INDEX IF NOT EXISTS `index_Source_name` ON `Source` (`name`);
 CREATE TABLE IF NOT EXISTS `Quote`
 (
     `quote_id`  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    `created`   INTEGER,
     `source_id` INTEGER,
     `text`      TEXT                              NOT NULL COLLATE NOCASE,
     FOREIGN KEY (`source_id`) REFERENCES `Source` (`source_id`) ON UPDATE NO ACTION ON DELETE SET NULL
